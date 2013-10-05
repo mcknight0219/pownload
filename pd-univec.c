fda1a79a85cf408a3ded353b9b405352e49bde56
Qiang Guo <mcknight0219@gmail.com>
2013-10-05 00:32:27 -0600

    fixed bugs of pd-univec's memory allocation

--- pd-univec.c
+++ pd-univec.c
@@ -1,11 +1,16 @@
 #include "pd-univec.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 struct pd_univec * pd_univec_new() {
-  struct pd_univec *uv = (struct pd_univec *)malloc(sizeof(*uv) + (PD_UNIVEC_DEFAULT_SIZE-1) * sizeof(void *));
+  struct pd_univec *uv = (struct pd_univec *)malloc(sizeof(*uv));
   if (!uv)
+    return 0;
+  uv->data = malloc(sizeof(void *) * PD_UNIVEC_DEFAULT_SIZE); 
+ 
+  if (!uv->data)
     return 0;
   uv->reserved = PD_UNIVEC_DEFAULT_SIZE;
   uv->size = 0;
@@ -16,32 +21,33 @@ int pd_univec_add(struct pd_univec *vec, void *elem, int (*cmp)(void *elem1, voi
   int res;
   int exist;
   if (vec->size + 1 == vec->reserved) {
-    vec = pd_univec_expand(vec);
-    if (vec == NULL) {
+    res = pd_univec_expand(vec);
+    if (res == -1) {
       fprintf(stderr, "Failed to expand vector.");
       return -1;
     }
-		printf("Expanding the vector\n");
+    printf("Expanding the vector\n");
   }
 
   // use provided comparision function
   if (!pd_univec_exist(vec, elem, cmp)) {
-    vec->data[vec->size] = elem;
+    memcpy((vec->data + (vec->size * sizeof(void *))), &elem, sizeof(void *));
     vec->size++;
   }
   return 0;
 }
 
-struct pd_univec * pd_univec_expand(struct pd_univec *vec) {
-	int new_rsv = vec->reserved << 1;
-	struct pd_univec * new_vec;
-	if (NULL == (new_vec = (struct pd_univec *)realloc(vec, sizeof(struct pd_univec) + (new_rsv-1) * sizeof(void *)))) {
-		fprintf(stderr, "Failed to realloc.");
-		return vec;
-	}
-
-	new_vec->reserved = new_rsv;
-	return new_vec;
+pd_univec_expand(struct pd_univec * vec) {
+  int new_rsv = vec->reserved << 1;
+  void * new_data;
+  if (NULL == (new_data = realloc(vec->data, new_rsv * sizeof(void *)))) {
+    fprintf(stderr, "Failed to realloc.");
+    return -1;
+  }
+	
+  vec->reserved = new_rsv;
+  vec->data = new_data;
+  return 0;
 }
   
 
@@ -53,7 +59,7 @@ int pd_univec_size(struct pd_univec * vec) {
 int pd_univec_exist(struct pd_univec *vec, void *elem, int (*cmp)(void *elem1, void *elem2)) {
   int i;
   for (i = 0; i < vec->size; ++i) {
-    if (!cmp(elem, vec->data[i]))
+    if (!cmp(elem, (void *)PD_UNIVEC_AT(vec, i)))
       return 1;
   }
   return 0;
@@ -61,15 +67,13 @@ int pd_univec_exist(struct pd_univec *vec, void *elem, int (*cmp)(void *elem1, v
 
 // free all the data as well
 void pd_univec_free(struct pd_univec *vec) {
-	int i;
-	if (!vec)
-		return;
-	
-	for (i = 0; i < vec->size; ++i) {
-		if (vec->data[i]) {
-			free(vec->data[i]);
-		}
-	}
-	free(vec);
-	return;
+  int i;
+  if (!vec)
+    return;
+  // TODO free the memory hold by vec->data
+  for (i = 0; i < vec->size; ++i) {
+    
+  }
+  free(vec);
+  return;
 }
--- pd-univec.h
+++ pd-univec.h
@@ -2,21 +2,21 @@
 #define _PD_UNIVEC_H
 
 #define PD_UNIVEC_DEFAULT_SIZE 16
-#define PD_UNIVEC_AT(v,type,i)							\
-	((type)(v->data[i]))
+#define PD_UNIVEC_AT(v,i)							\
+  (*(long *)(v->data + i * sizeof(void *)))
 
 struct pd_univec {
-
   int size;
   int reserved;
-  void *data[1];
+  void * data;
 };
 
 struct pd_univec * pd_univec_new();
 int pd_univec_add(struct pd_univec *vec, void *elem, int (*cmp)(void *elem1, void *elem2));
-struct pd_univec * pd_univec_expand(struct pd_univec *vec);
+int pd_univec_expand(struct pd_univec *vec);
 int pd_univec_size(struct pd_univec *vec);
 int pd_univec_exist(struct pd_univec *vec, void *elem, int (*cmp)(void *elem1, void *elem2));
 void pd_univec_free(struct pd_univec *vec);
+
 
 #endif
